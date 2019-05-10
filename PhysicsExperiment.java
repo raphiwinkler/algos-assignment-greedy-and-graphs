b5bcdaaae2d0460e0f890400db437afa5e03391a
raphiwinkler <25161574+raphiwinkler@users.noreply.github.com>
2019-05-10 18:52:53 -0400

    Gave Up On Fixing Indentation Again

--- PhysicsExperiment.java
+++ PhysicsExperiment.java
@@ -56,6 +56,7 @@ public class PhysicsExperiment {
 	    		for(int i = s; i <= numSteps; i++){
 
             // Increase count if available, else break 
+            //(there may be indentation problems here on GH that I can't fix for some reason)
             if(signUpTable[j][i] == 1){
 	    				count++;
 	    			}else{
@@ -63,8 +64,9 @@ public class PhysicsExperiment {
 	    			}
 	    		}
 
-            // Keep track of the student available for the most successive next steps
-            if(count > max){
+          // Keep track of the student available for the most successive next steps
+          //(again, there may be indentation problems here on GH that I can't fix for some reason)
+          if(count > max){
   					max = count;
   					top = j;
 				  }
